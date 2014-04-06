877f3eba9d0a3a8025244a6a4dae2fa8e5e3bff1
fwsgonzo <fwsgonzo@hotmail.com>
2014-04-06 22:24:12 +0200

    Centered window

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,19 @@
 #include <time.h>
 
 static const int FPS = 60;
-static const int PARTICLES = 150;
-
-static const int WINDOW_W  = 800;
-static const int WINDOW_H  = 600;
+static const int PARTICLES = 10;
 
 #include "canvas.hpp"
 
 int main()
 {
 	srand(time(0));
+	const int WINDOW_W  = Fl::w() * 0.75;
+	const int WINDOW_H  = Fl::h() * 0.75;
+	const int WINDOW_X  = (Fl::w() / 2) - (WINDOW_W / 2);
+	const int WINDOW_Y  = (Fl::h() / 2) - (WINDOW_H / 2);
 	
-	Fl_Double_Window win(WINDOW_W, WINDOW_H);
+	Fl_Double_Window win(WINDOW_X, WINDOW_Y, WINDOW_W, WINDOW_H);
 	Canvas canvas(0, 0, WINDOW_W, WINDOW_H, PARTICLES, FPS);
 	
 	win.end();
